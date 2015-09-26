646e65cfa41be25f8d4609b4674f4f416e4e062c
Ottony Costa <ottony93@gmail.com>
2015-09-26 16:09:28 -0300

    Creates seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+attractions = [
+  {
+    title: 'True Detective',
+    media: 'television',
+    day: 1.hour.from_now
+  },
+  {
+    title: 'interstellar',
+    media: 'cinema',
+    day: 1.day.from_now
+  },
+  {
+    title: 'Is TDD dead?',
+    media: 'Hangout',
+    day: 8.hours.from_now
+  },
+]
+
+user = User.create( email: 'example@email.com', password: 'password', nick_name: 'OCTC' )
+user.attractions.create( attractions )
