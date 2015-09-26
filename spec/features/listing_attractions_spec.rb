47f32aee75df992b7ad2c8bd2daca0c79e9fc167
Ottony Costa <ottony93@gmail.com>
2015-09-25 22:23:08 -0300

    List all attraction temporarily

--- spec/features/listing_attractions_spec.rb
+++ spec/features/listing_attractions_spec.rb
@@ -3,5 +3,16 @@ require 'rails_helper'
 feature 'Listing attraction' do
   background do
     create_list :attraction, 5
+    visit attractions_path
+  end
+
+  scenario 'show titles' do
+    Attraction.all.each do |attraction|
+      expect(page).to have_content attraction.title
+    end
+  end
+
+  scenario 'show formated date' do
+    expect(page).to have_content I18n.l(Attraction.last.day, :format => :short)
   end
 end
