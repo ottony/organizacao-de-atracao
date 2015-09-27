9abd9429f07afb9d63940c12031eb10c32e2347f
Ottony Costa <ottony93@gmail.com>
2015-09-27 18:46:48 -0300

    Use show attractions action based on permission

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,4 +36,28 @@ module ApplicationHelper
 
     link_to nick_name, attractions_user_path( attraction.user_id ), args
   end
+
+  def attraction_actions_links attraction
+    edit_attraction_link(attraction).concat( delete_attraction_link(attraction) )
+  end
+
+  private
+
+  def edit_attraction_link attraction
+    return '' unless can? :edit, attraction
+
+    link_to edit_attraction_path( attraction ) do
+            content_tag(:span, nil, class: 'fa fa-edit')
+    end 
+  end
+
+  def delete_attraction_link attraction
+    return '' unless can? :destroy, attraction
+
+    link_to attraction_path( attraction ),
+      :method => :delete,
+      data: { confirm: "Você tem certeza?" } do
+            content_tag(:span, nil, class: 'fa fa-trash-o')
+    end
+  end
 end
