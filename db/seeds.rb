523dee8a73f1e50ab7f500dddcc508b9c9b6dc0b
Ottony Costa <ottony93@gmail.com>
2015-09-27 22:14:59 -0300

    Updates seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,33 +10,39 @@ attractions = [
   {
     title: 'True Detective',
     media: 'TV',
-    day: 1.hour.from_now,
+    day: 30.hour.from_now,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non auctor mauris.'
   },
   {
     title: 'interstellar',
     media: 'cinema',
-    day: 1.day.from_now,
+    day: 2.day.from_now,
     description: 'Aenean massa nisi, pharetra in sodales at, elementum vitae metus.'
   },
   {
     title: 'Breaking Bad',
     media: 'TV',
-    day: 8.hours.from_now,
+    day: 50.hours.from_now,
     description: 'Phasellus non imperdiet enim. Etiam a suscipit lectus.'
   },
   {
     title: 'System of a Down',
     media: 'YouTube',
-    day: 2.day.from_now,
+    day: 3.day.from_now,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
   {
     title: 'Notch Coding',
     media: 'Twitch',
     day: 10.days.from_now,
-    description: 'Curabitur scelerisque ligula ut ante molestie, ut fringilla ex condimentum. Quisque aliquam nulla sed mi cursus tincidunt. '
+    description: 'Curabitur scelerisque ligula ut ante molestie, ut fringilla ex condimentum.'
   },
+  {
+    title: 'House Of Cards',
+    media: 'TV',
+    day: 5.days.from_now,
+    description: 'Curabitur scelerisque ligula ut ante molestie, ut condimentum.'
+  }
 ]
 
 user = User.create( email: 'example@email.com', password: 'password', nick_name: 'OCTC' )
