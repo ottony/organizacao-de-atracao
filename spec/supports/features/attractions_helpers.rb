0a821af3b3c8e900f2d677584fbd0024b7cd10fd
Ottony Costa <ottony93@gmail.com>
2015-09-27 21:50:46 -0300

    Fix feature test

--- spec/supports/features/attractions_helpers.rb
+++ spec/supports/features/attractions_helpers.rb
@@ -1,11 +1,7 @@
 module Features
   module AttractionsHelpers
     def select_datetime(date, from: )
-      select I18n.l( date, format: '%Y'), :from => "#{from}_1i"
-      select I18n.l( date, format: '%B'), :from => "#{from}_2i"
-      select I18n.l( date, format: '%d'), :from => "#{from}_3i"
-      select I18n.l( date, format: '%H'), :from => "#{from}_4i"
-      select I18n.l( date, format: '%M'), :from => "#{from}_5i"
+      fill_in 'attraction[day]', with: Time.now
     end
   end
 end
